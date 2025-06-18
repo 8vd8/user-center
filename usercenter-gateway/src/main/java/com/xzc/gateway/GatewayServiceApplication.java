b74c309ec1cb8caa8a900600976779b8553238ff
8vd8 <1936002261@qq.com>
2025-06-18 15:46:25 +0800

    add(): 1. 在网关中增加了限流的配置 2. 在用户模块中增加了日志重试机制 fix(): 1.总模块中移除了common模块

--- usercenter-gateway/src/main/java/com/xzc/gateway/GatewayServiceApplication.java
+++ usercenter-gateway/src/main/java/com/xzc/gateway/GatewayServiceApplication.java
@@ -1,14 +1,19 @@
 package com.xzc.gateway;
 
+import com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 网关服务启动类
  * @author xzc
  */
-@SpringBootApplication
+@SpringBootApplication(exclude ={
+DataSourceAutoConfiguration .class,
+MybatisPlusAutoConfiguration .class
+})
 @EnableDiscoveryClient
 public class GatewayServiceApplication {
     
